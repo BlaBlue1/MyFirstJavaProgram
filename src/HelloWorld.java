ad19fefa8888ef01900f1fbb29f6d41a9f96ddf4
RuthWudu <ruthwudu576@gmail.com>
2021-07-09 04:40:45 -0400

    descriptive message

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Today is July 9, 2021. I am alive!");
+        System.out.println("My name is Ruth");
+    }
+}
