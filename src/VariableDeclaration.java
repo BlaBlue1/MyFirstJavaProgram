6be83973148e341c062d283ab68d455b1d53b250
RuthWudu <ruthwudu576@gmail.com>
2021-07-09 05:58:53 -0400

    descriptive message

---
+++ src/VariableDeclaration.java
@@ -0,0 +1,46 @@
+public class VariableDeclaration {
+    public static void main(String[] args) {
+        //declare variables here before we use them
+            int x, y, answer;
+         //all three variables will be declared as integers
+         double temperature;
+         float Temperature;
+         float GPA;
+         //a float uses less memory than a double
+            String firstName, lastName;
+            String question = "unknown"; //Question is initialized </p>
+            String lastMovie;
+
+             //assign values to the variables here
+         x = 99;
+         y = 2147483647; //you could also use the constant Integer.MAX_VALUE
+         answer = 34;
+         GPA = 3.78f;
+         firstName = "Ruth ";
+         lastName = "Tadese";
+         // String question = "unknown"; //question is initialized
+         temperature = 98.6;
+         Temperature = 32.0f;
+         lastMovie = "Awake";
+
+
+         //Use the variables here
+        System.out.println("The variable x contains a value of " + x );
+        System.out.println("The value " + y + " is the largest value ");
+        System.out.println("you can store in an integer.");
+        System.out.println("The answer to the question is: " + answer);
+        System.out.println("The lastMovie that i saw is" + lastMovie);
+        System.out.println("And the question has long since been " + question);
+        System.out.println("If you're not sick your temperature is " + temperature);
+        System.out.println("If you're an ice cube your temperature is " + Temperature);
+        System.out.println("The variable Temperature is not " + " the same as temperature");
+        System.out.println("The founder of Java is " + firstName + lastName);
+        System.out.println("My GPA " + GPA);
+
+
+
+
+
+
+    }
+}
